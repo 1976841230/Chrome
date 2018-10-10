b4827c8c2585b9c7da8c5922827ef551473b236e
Xinling Jing <jingxinling1024@163.com>
2018-10-10 19:33:23 +0800

    天气

---
+++ weather/js/options.js
@@ -0,0 +1,5 @@
+var city = localStorage.city || "shanghai";
+document.getElementById('city').value = city;
+document.getElementById("save").onclick = function () {
+    localStorage.city = document.getElementById("city").value;
+};
